633a4cd51db5230d282693abdb4aca3f0eb7e1f1
eithnahex <38161732+eithnahex@users.noreply.github.com>
2023-03-13 20:01:59 +0500

    setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='newpipe_importer',
+      version='1.0',
+      description='Newpipe import helper',
+      author='eithnahex',
+      author_email='',
+      url='https://github.com/eithnahex/newpipe_importer',
+      )
